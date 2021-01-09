249cf2889c4582af740f58709ffb67f5ec390e5f
Amy Cai <amycai89@gmail.com>
2021-01-08 17:46:47 -0800

    Updates

---
+++ precache-manifest.bd77cf08f975de3e4e3f92aafa9fad12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5c9c60943436319ecac569f549762c",
+    "url": "/Blackjack/index.html"
+  },
+  {
+    "revision": "191bbcd96c25c1123a06",
+    "url": "/Blackjack/static/css/main.00e849a1.chunk.css"
+  },
+  {
+    "revision": "fdf741498779bf01aa3d",
+    "url": "/Blackjack/static/js/2.675d0209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Blackjack/static/js/2.675d0209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191bbcd96c25c1123a06",
+    "url": "/Blackjack/static/js/main.ff970602.chunk.js"
+  },
+  {
+    "revision": "1d446929c628bee59553",
+    "url": "/Blackjack/static/js/runtime-main.242636f9.js"
+  }
+]);
